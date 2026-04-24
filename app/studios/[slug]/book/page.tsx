5ec0b1cca29f0da89df6a8d361e869899fd4399d
ialsharif87 <i.a.alsharif@gmail.com>
2026-04-24 01:58:03 -0700

    Update page.tsx

--- app/studios/[slug]/book/page.tsx
+++ app/studios/[slug]/book/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "../../../../lib/supabase/server";
 
 export default async function BookStudioPage({
   params
